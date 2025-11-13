25418cabb98c06989bad1a6c0c20f4fcefa9e91b
GIOVANNA COELHO GILIO DE SA <24122381@dsn127m1242313.cfp127aluno.local>
2025-11-13 17:10:20 -0300

    fix: bugs

--- app/src/main/java/com/example/analyticai/service/DesempenhoService.kt
+++ app/src/main/java/com/example/analyticai/service/DesempenhoService.kt
@@ -1,12 +1,23 @@
 package com.example.analyticai.service
 
 import com.example.analyticai.model.Dashboard.DashboardResponse
+import com.example.analyticai.model.Login.LoginRequest
+import com.example.analyticai.model.Login.LoginResponse
+import com.example.analyticai.model.Login.Usuario
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface DesempenhoService {
-    @GET("/v1/analytica-ai/aluno/{id}")
+    @POST("usuarios/login")
+    suspend fun getAluno(@Body loginRequest: LoginRequest): LoginResponse
+
+    @GET("desempenho/aluno/{idAluno}")
     suspend fun getDesempenho(
-        @Path("id") alunoId: String
+        @Path("idAluno") idAluno: Int,
+        @Query("materia") idMateria: Int? = null,
+        @Query("semestre") idSemestre: Int? = null
     ): DashboardResponse
 }
